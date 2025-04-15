a5857d7314b56dc493c572474069a375addd03ed
ejjays <christsonalloso021@gmail.com>
2025-04-15 12:46:20 +0800

    Auto commit

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -415,25 +415,25 @@ export const FacebookIcon = ({ className, ...rest }) => {
       xmlns="http://www.w3.org/2000/svg"
       x="0px"
       y="0px"
-      width={24} // Change from 100 to 24
-      height={24} // Change from 100 to 24
-      viewBox="0 0 48 48"
+      width={24}
+      height={24}
+      viewBox="0 0 42 42" // Changed viewBox to match other icons
       className={cx("w-full h-auto", className)}
       {...rest}
     >
       <g clipPath="url(#a)">
         <path
           fill="#039be5"
-          d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"
+          d="M21 5A16 16 0 1 0 21 37A16 16 0 1 0 21 5Z"
         />
         <path
           fill="#fff"
-          d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
+          d="M23.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"
         />
       </g>
       <defs>
         <clipPath id="a">
-          <path fill="#fff" d="M0 0h48v48H0z" />
+          <path fill="#fff" d="M0 0h42v42H0z" />
         </clipPath>
       </defs>
     </svg>
