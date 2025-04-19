6e1a27e96f05e964f063ec374c135343b7c69c22
ejjays <christsonalloso021@gmail.com>
2025-04-19 19:36:12 +0800

    Updated PWA metadata

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,8 +1,8 @@
 export default function manifest() {
     return {
-      name: 'Next.js App',
-      short_name: 'Next.js App',
-      description: 'Next.js App',
+      name: 'PCMI - Infanta',
+      short_name: 'Pag-ibig Christian Ministries — Infanta',
+      description: 'Official Website of PCMI -Infanta',
       start_url: '/',
       display: 'standalone',
     //   background_color: '#fff',
