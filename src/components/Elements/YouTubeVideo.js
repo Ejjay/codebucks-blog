c2bb9e9ddb3f131ab38b3489bb4679b2cd42558e
ejjays <christsonalloso021@gmail.com>
2025-04-23 16:11:42 +0800

    Test Override Hide YouTube watermark

--- src/components/Elements/YouTubeVideo.js
+++ src/components/Elements/YouTubeVideo.js
@@ -2,12 +2,27 @@ const YouTubeVideo = ({ videoId, title = "YouTube Video", isBanner = false }) =>
   return (
     <div className={`relative w-full ${isBanner ? 'h-[60vh]' : 'pb-[56.25%] h-0'} overflow-hidden max-w-full`}>
       <iframe
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={`https://www.youtube.com/embed/${videoId}?rel=0&modestbranding=1&controls=1&showinfo=0&fs=0`}
         title={title}
         className="absolute top-0 left-0 w-full h-full"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        style={{
+          pointerEvents: 'auto'
+        }}
       />
+      <style jsx global>{`
+        .ytp-chrome-top-buttons,
+        .watch-on-youtube-button-wrapper,
+        .ytp-watermark,
+        .ytp-youtube-button,
+        .ytp-button {
+          display: none !important;
+          opacity: 0 !important;
+          visibility: hidden !important;
+          pointer-events: none !important;
+        }
+      `}</style>
     </div>
   );
 };
